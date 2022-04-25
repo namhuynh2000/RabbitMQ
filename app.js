a96d79b2894863ac0342b3f654af185e0be9ff4b
Duc Loc <ducloc0009@gmail.com>
2022-04-25 12:19:39 +0700

    Back end

---
+++ app.js
@@ -0,0 +1,44 @@
+import rabbitmq from "./connection/rabbitmq/amqp.js";
+import CRUD from "./model/film.model.js";
+
+rabbitmq("rpc_queue", { durable: false })
+  .then(async (value) => {
+    const { channel, queue } = value;
+    console.log("connected to rabbitmq");
+    channel.consume(queue, async function reply(msg) {
+      const req = JSON.parse(msg.content);
+
+      //   Handle request here
+      console.log(req);
+      var { type } = req;
+      console.log("type: ", type);
+      let data;
+      switch (type) {
+        case "create":
+          data = await CRUD().Create(req.entity);
+          break;
+        case "read":
+          data = await CRUD().Read();
+          break;
+        case "update":
+          data = await CRUD().Update(req.film_id, req.entity);
+          break;
+        case "delete":
+          data = await CRUD().Delete(req.film_id);
+          break;
+        default:
+          throw new Error("No valid type");
+      }
+      channel.ack(msg);
+
+      //   Send response
+      channel.sendToQueue(
+        msg.properties.replyTo,
+        Buffer.from(JSON.stringify(data)),
+        {
+          correlationId: msg.properties.correlationId,
+        }
+      );
+    });
+  })
+  .catch((e) => console.log(e));
--- connection/database/db.js
+++ connection/database/db.js
@@ -1,14 +1,16 @@
-import knex from 'knex';
+import knex from "knex";
 
 export default knex({
-  client: 'pg',
-  version: '14.2',
+  client: "mysql2",
   connection: {
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'postgres',
-    password: 'kiki123',
-    database: 'sakila-data'
+    host: "localhost",
+    post: 3306,
+    user: "root",
+    password: "root",
+    database: "sakila",
   },
-  pool: { min: 0, max: 10 }
+  pool: {
+    min: 0,
+    max: 10,
+  },
 });
